5f8275a624e85f13d1b07d506ce39c66d27a47ec
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2019-01-22 13:09:29 +0100

    build script for addog *.so added

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ID=ti.fidel
+VERSION=1.0.0
+MODULENAME=$ID-android-$VERSION.zip
+LIBS=android/nativelibs
+MODULE=android/dist/$MODULENAME
+ARCHS=( "arm64-v8a" "armeabi-v7a" "x86" )
+rm $MODULE
+ti build -b -p android -d android
+cd android/dist/
+unzip  *.zip
+for a in "${ARCHS[@]}"
+do 
+   cp -v ../../$LIBS/$a/*   modules/android/$ID/$VERSION/libs/$a/	
+done
+zip -umr $MODULENAME modules/
+cd ../../
